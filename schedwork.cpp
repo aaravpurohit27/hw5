984469c580d5d1ad5194ae0e0c3a1a133e571aa9
Aarav Purohit <aaravpur@usc.edu>
2025-12-05 19:10:14 +0000

    Checkpoint reached, all tests passed, wordle tests run a bit long

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool assign(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<int>& nShift, size_t nDay);
+bool valWorker(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<int>& nShift, size_t nDay);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,11 +36,72 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
-    sched.clear();
+    
     // Add your code below
 
+  //backtracking recursive algorithm that makes a n row by k column matrix of booleans
+  //d workers needed
+  //k possible workers
+  //n day period
+  //m max max shifts a woker can work over the n day period
 
+  size_t d = dailyNeed;
+  size_t m = maxShifts;
+  size_t n = avail.size();//row
+  size_t k = avail[0].size();//column
+
+  sched.clear();
+  sched.resize(n);
+
+  vector<int> nShift(k, 0);
+
+  return assign(avail, dailyNeed, maxShifts, sched, nShift, 0);
 
 
 }
 
+bool assign(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<int>& nShift, size_t nDay){
+  size_t n = avail.size();
+
+  //base case
+  //all days filled
+  if(nDay == n){
+    return true;
+  }
+
+  //move to next day if curr day is not available
+  if(sched[nDay].size() == dailyNeed){
+    return assign(avail, dailyNeed, maxShifts, sched, nShift, nDay+1);
+  }
+
+  return valWorker(avail, dailyNeed, maxShifts, sched, nShift, nDay);
+  //new recur func
+
+}
+
+
+bool valWorker(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<int>& nShift, size_t nDay){
+  size_t k = avail[nDay].size();
+
+  for(size_t i=0; i<k; i++){
+    if(!avail[nDay][i]){
+      continue;
+    }
+    if(nShift[i]<maxShifts){
+      if(find(sched[nDay].begin(), sched[nDay].end(), i) != sched[nDay].end()){
+        continue;
+      }
+      sched[nDay].push_back(i);
+      nShift[i]++;
+
+      //fill next worker or day
+      if(assign(avail, dailyNeed, maxShifts, sched, nShift, nDay)==true){
+        return true;
+      }
+      //btrack 
+      sched[nDay].pop_back();
+      nShift[i]--;
+    }
+  }
+  return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,16 +13,97 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
-
+void getWords(const std::string& in, const std::string& floating, const std::set<std::string>& dict, int windex, std::string& word, std::set<std::string>& palabras);
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+  // Add your code here
+  
+  std::set<std::string> palabras;
+  std::string word = in;
 
+  getWords(in, floating, dict, 0, word, palabras);
+  return palabras;
 }
 
 // Define any helper functions here
+void getWords(const std::string& in, const std::string& floating, const std::set<std::string>& dict, int windex, std::string& word, std::set<std::string>& palabras){
+  //le base case
+  //index is at last pos of input and all floating letters have been used
+  int sze = in.size();
+
+  if(windex == sze){
+    if(floating.empty() == true){//all floating used
+      if(dict.find(word) != dict.end()){//find if word exists by using .find() in the dict
+        palabras.insert(word);
+      }
+    }
+    return;
+  }
+
+  int b = 0;
+  for(int i=windex; i<sze; i++){
+    if(in[i]=='_'||in[i]=='-'){
+      b++;
+    }
+  }
+
+  if(floating.size() > b){
+    return;
+  }
+
+  //case where da letter is green
+  if(in[windex] != '_' && in[windex]!='-'){
+    word[windex] = in[windex];
+    getWords(in, floating, dict, windex+1, word, palabras);
+    return;
+  }
+
+
+
+
+  for(int l=0; l<floating.size(); l++){
+    char let = floating[l];
+    word[windex] = let;
+    string floatl = floating;
+    floatl.erase(l, 1);
+
+    getWords(in, floatl, dict, windex+1, word, palabras);
+  }
+
+
+  if(floating.size() == b){
+    return;
+  }
+
+  //case where letter slot is empty "_"
+  for(char alph = 'a'; alph<='z'; alph++){
+
+    word[windex] = alph;
+  
+    //check if letter is in floating
+    int findex = floating.find(alph);
+
+    if(findex != -1){
+      // string eFloat = floating;
+      // eFloat.erase(findex, 1);
+      // getWords(in, eFloat, dict, windex+1, word, palabras);
+      continue;
+    }
+    getWords(in, floating, dict, windex+1, word, palabras);
+    // else{
+    //   int numb = 0;
+    //   for(int i=windex+1; i<sze; i++){
+    //     if(in[i]=='_'||in[i]=='-'){
+    //       numb++;
+    //     }
+    //   }
+    //   if(floating.size() <= numb){
+    //     getWords(in, floating, dict, windex+1, word, palabras);
+    //   }
+    // }
+  }
+}
